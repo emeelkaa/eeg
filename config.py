f894ea06656e050cd1206574236ae29fd7fb3891
emeelkaa <barcaonelove1899@gmail.com>
2025-04-22 10:26:01 +0900

    Added config file

---
+++ config.py
@@ -0,0 +1,11 @@
+NORMALIZE = 1 
+FILTER = 1 
+STEP_SIZE = 10 
+WINDOW_SIZE = 10 
+ 
+PARAMETERS = [ 
+    [0, 1], # Normalize 
+    [0, 1], # Filter 
+    [4, 10, 30] # Window size 
+] 
+SAMPLING_RATE = 256
